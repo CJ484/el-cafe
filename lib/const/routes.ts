4f5ad8ec20385b4f03e223a42dc3fd157dda1e1a
Carlos Coreas <ccoreas484@gmail.com>
2025-06-29 23:07:51 -0400

    Add routes file to define header navigation links for Home, Menu, About Us, and Contact Us

---
+++ lib/const/routes.ts
@@ -0,0 +1,18 @@
+export const headerLinks: { label: string, href: string }[] = [
+    {
+        label: 'Home',
+        href: '/'
+    },
+    {
+        label: 'Menu',
+        href: '/menu'
+    },
+    {
+        label: 'About Us',
+        href: '/about-us'
+    },
+    {
+        label: 'Contact Us',
+        href: '/contact-us'
+    }
+]
