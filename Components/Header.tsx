33f2f609fb3569b68421ccff836d01ee68157a02
Carlos Coreas <ccoreas484@gmail.com>
2025-06-29 23:04:57 -0400

    Refactor Header component to use responsive navigation and updated styles

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,37 +1,24 @@
+'use client'
 import React from 'react'
+import { useWindowSize } from 'usehooks-ts';
 import Link from 'next/link';
-
-const headerLinks: { label: string, href: string }[] = [
-    {
-        label: 'Home',
-        href: '/'
-    },
-    {
-        label: 'Menu',
-        href: '/menu'
-    },
-    {
-        label: 'About Us',
-        href: '/about-us'
-    },
-    {
-        label: 'Contact Us',
-        href: '/contact-us'
-    }
-]
+import { ShoppingCartIcon } from 'lucide-react';
+import { DesktopNav, MobileNav } from '@/components/Navigation';
+import { Button } from '@/components/ui/button';
+import styles from '@/styles/components.module.scss';
 
 export default function Header() {
+    const isMobile = useWindowSize();
+
   return (
-    <header>
-        <h1>El Cafe</h1>
-        <nav>
-           {headerLinks.map((link) => (
-            <Link key={link.label} href={link.href}>
-                {link.label}
+    <header className={styles.header}>
+        <Link href="/" className={styles.header__logo}>El Cafe</Link>
+        {isMobile.width < 768 ? <MobileNav /> : <DesktopNav />}
+        <Button asChild size="sm" className={styles.header__orderButton}>
+            <Link href="/">                
+                <ShoppingCartIcon /> Order Now
             </Link>
-           ))} 
-        </nav>
-        
+        </Button>
     </header>
   )
 }
